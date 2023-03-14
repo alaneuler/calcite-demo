ec5fa3ec7def4ea8e33e5434b216f7c3d84f071a
alaneuler <alaneuler@qq.com>
2023-03-14 14:33:21 +0800

    finish agg mains

---
+++ src/main/java/me/alaneuler/calcite/ng/demo/agg/AggregateReduceMain.java
@@ -0,0 +1,27 @@
+package me.alaneuler.calcite.ng.demo.agg;
+
+import me.alaneuler.calcite.ng.demo.util.CommonTableMain;
+import me.alaneuler.calcite.ng.demo.util.RelUtils;
+import org.apache.calcite.plan.hep.HepPlanner;
+import org.apache.calcite.plan.hep.HepProgramBuilder;
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.rules.CoreRules;
+
+public class AggregateReduceMain extends CommonTableMain {
+  public static void main(String[] args) {
+    String sql = """
+        SELECT name, stddev(age), avg(age) from pt_user group by name
+        """;
+    RelNode relNode = RelUtils.toRel(sql);
+
+    HepPlanner hepPlanner = hepPlanner();
+    hepPlanner.setRoot(relNode);
+    relNode = hepPlanner.findBestExp();
+  }
+
+  private static HepPlanner hepPlanner() {
+    HepProgramBuilder builder = new HepProgramBuilder();
+    builder.addRuleInstance(CoreRules.AGGREGATE_REDUCE_FUNCTIONS);
+    return new HepPlanner(builder.build());
+  }
+}
--- src/main/java/me/alaneuler/calcite/ng/demo/agg/AggregateUnionMain.java
+++ src/main/java/me/alaneuler/calcite/ng/demo/agg/AggregateUnionMain.java
@@ -1,24 +1,23 @@
 package me.alaneuler.calcite.ng.demo.agg;
 
 import me.alaneuler.calcite.ng.demo.util.CommonTableMain;
-import me.alaneuler.calcite.ng.demo.util.RboOptimize;
 import me.alaneuler.calcite.ng.demo.util.RelUtils;
 import org.apache.calcite.plan.hep.HepPlanner;
 import org.apache.calcite.plan.hep.HepProgramBuilder;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.rules.CoreRules;
 
-public class Main extends CommonTableMain {
+public class AggregateUnionMain extends CommonTableMain {
   public static void main(String[] args) {
     String sql = """
         SELECT user_id,
                goods,
                sum(price)
         FROM (
-          SELECT user_id, goods, price
+          SELECT price, goods, user_id
           FROM pt_order WHERE id < 100
           UNION ALL
-          SELECT user_id, goods, price
+          SELECT price, goods, user_id
           FROM pt_order WHERE id > 1000
         )
         GROUP BY user_id, goods
