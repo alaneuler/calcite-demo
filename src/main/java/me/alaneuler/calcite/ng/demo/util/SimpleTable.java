085aa2baec65f99825bc617e10790479da826d52
alaneuler <alaneuler@qq.com>
2023-04-04 19:50:23 +0800

    new

--- src/main/java/me/alaneuler/calcite/ng/demo/util/SimpleTable.java
+++ src/main/java/me/alaneuler/calcite/ng/demo/util/SimpleTable.java
@@ -1,18 +1,21 @@
 package me.alaneuler.calcite.ng.demo.util;
 
+import org.apache.calcite.DataContext;
+import org.apache.calcite.linq4j.Enumerable;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rel.type.RelDataTypeField;
 import org.apache.calcite.rel.type.RelDataTypeFieldImpl;
 import org.apache.calcite.rel.type.RelRecordType;
 import org.apache.calcite.rel.type.StructKind;
+import org.apache.calcite.schema.ScannableTable;
 import org.apache.calcite.schema.impl.AbstractTable;
 import org.apache.calcite.sql.type.SqlTypeName;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimpleTable extends AbstractTable {
+public class SimpleTable extends AbstractTable implements ScannableTable {
   private List<String> fieldNames = new ArrayList<>();
   private List<SqlTypeName> fieldTypes = new ArrayList<>();
 
@@ -38,5 +41,10 @@ public class SimpleTable extends AbstractTable {
   public void addField(String name, SqlTypeName typeName) {
     fieldNames.add(name);
     fieldTypes.add(typeName);
+  }
+
+  @Override
+  public Enumerable<Object[]> scan(DataContext root) {
+    return null;
   }
 }
--- src/main/java/me/alaneuler/calcite/ng/demo/volcano/Main.java
+++ src/main/java/me/alaneuler/calcite/ng/demo/volcano/Main.java
@@ -2,9 +2,12 @@ package me.alaneuler.calcite.ng.demo.volcano;
 
 import me.alaneuler.calcite.ng.demo.util.CommonTableMain;
 import me.alaneuler.calcite.ng.demo.util.RelUtils;
+import me.alaneuler.calcite.ng.demo.util.TableUtils;
 import me.alaneuler.calcite.ng.demo.util.VolcanoUtils;
 import org.apache.calcite.adapter.enumerable.EnumerableConvention;
+import org.apache.calcite.adapter.enumerable.EnumerableRules;
 import org.apache.calcite.plan.RelOptCluster;
+import org.apache.calcite.plan.RelOptPlanner;
 import org.apache.calcite.plan.RelOptUtil;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.plan.volcano.VolcanoPlanner;
@@ -14,21 +17,32 @@ import org.apache.calcite.sql.SqlExplainLevel;
 public class Main extends CommonTableMain {
   public static void main(String[] args) {
     String sql = """
-        SELECT * FROM pt_user
+        SELECT * FROM pt_user WHERE id = 1
         """;
 
     RelNode rel = RelUtils.sqlToRel(sql);
+    System.out.println("Explain after creating:");
     System.out.println(RelOptUtil.toString(rel, SqlExplainLevel.ALL_ATTRIBUTES));
 
     VolcanoPlanner planner = VolcanoUtils.extractVolcanoPlanner(rel);
+    addEnumerableRules(planner);
     RelOptCluster cluster = rel.getCluster();
 
     RelTraitSet traits = cluster.traitSet().replace(EnumerableConvention.INSTANCE);
     rel = planner.changeTraits(rel, traits);
+    System.out.println("Explain after changing traits:");
     System.out.println(RelOptUtil.toString(rel, SqlExplainLevel.ALL_ATTRIBUTES));
 
     planner.setRoot(rel);
-    planner.findBestExp();
     VolcanoUtils.dump(planner);
+
+    RelNode best = planner.findBestExp();
+    VolcanoUtils.dump(planner);
+
+    
+  }
+
+  private static void addEnumerableRules(RelOptPlanner planner) {
+    EnumerableRules.rules().forEach(planner::addRule);
   }
 }
